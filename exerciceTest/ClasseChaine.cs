11f58a746ec14f870055331336186566e14d015c
AymerichDelettre <33418072+AymerichDelettre@users.noreply.github.com>
2020-06-21 23:01:17 +0200

    Init Repo !

---
+++ exerciceTest/ClasseChaine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciceTest
+{
+    class ClasseChaine
+    {
+        public static string TraiterTexte(String inputText)
+        {
+            String initiales = "";
+            if (string.IsNullOrEmpty(inputText) || inputText.Length < 3)
+            {
+                return string.Empty;
+            }
+            if (inputText.Contains(" "))
+            {
+                String[] content = inputText.Split(' ');
+                if (content.Length != 2)
+                {
+                    return string.Empty;
+                }
+                initiales = content[0][0].ToString() + "." + content[1][0].ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
+            
+            return initiales;
+        }
+    }
+}
